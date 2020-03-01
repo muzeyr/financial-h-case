cebf2c2645871dfdb04488779bf41b2c1d1bc27a
M Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-01 14:55:44 +0300

    SErver cors is fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://sandbox-reporting.rpdpymnt.com/api/v3/',
-//  url: 'https://reporting.rpdpymnt.com/api/v3/merchant/'
+//  url: 'https://sandbox-reporting.rpdpymnt.com/api/v3/',
+ url: 'https://reporting.rpdpymnt.com/api/v3/merchant/'
 };
