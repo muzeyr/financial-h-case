43addee2c530e0f02899e7c82cc6ca58ce8419da
M Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-01 16:05:06 +0300

    SErver cors is fixed

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ var corsOptions = {
     origin: 'https://sandbox-reporting.rpdpymnt.com',
     optionsSuccessStatus: 200
   }
-app.use(express.static(__dirname + '/dist/financial-case'));
-
 app.get('/*',cors(corsOptions), function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/financial-case/index.html'));
 });
