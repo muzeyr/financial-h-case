5e91dd628ccf0874cf637d52fff3488579f87edd
M Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-01 12:56:31 +0300

    Cors

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+var cors = require('cors');
 var corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
