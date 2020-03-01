5a03dcad6447fc6cc001b36e032f500e385dbd1b
M Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-01 16:11:01 +0300

    SErver cors is fixed

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app = express();
 var cors = require('cors');
 var sslRedirect = require('heroku-ssl-redirect');
 const app = express();
-app.use(sslRedirect());
+//app.use(sslRedirect());
 var corsOptions = {
     origin: 'https://sandbox-reporting.rpdpymnt.com',
     optionsSuccessStatus: 200
   }
+app.use(express.static(__dirname + '/dist/financial-case'));
+
 app.get('/*',cors(corsOptions), function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/financial-case/index.html'));
 });
