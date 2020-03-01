4796e37fc3636ce7cdeda8f5bc99043999a69471
Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-01 22:51:53 +0300

    Comp

--- src/app/moduls/helps/jwt.interceptor.ts
+++ src/app/moduls/helps/jwt.interceptor.ts
@@ -21,6 +21,7 @@ export class JwtInterceptor implements HttpInterceptor {
                     }
                 });
             } else {
+                console.log('Access....');
                 request = request.clone({
                     setHeaders: {
                         Accept : '*',
