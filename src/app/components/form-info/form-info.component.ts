f65acdee2ed9ab9073815df5a7b735b2dbe3b2fe
Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-10 13:06:48 +0300

    Tslint fixed

--- src/app/components/form-info/form-info.component.ts
+++ src/app/components/form-info/form-info.component.ts
@@ -20,38 +20,36 @@ export class FormInfoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let _controller = this;
-    if(this.info){
+    const controller = this;
+    if (this.info) {
       console.log(this.info);
       this.lbl = Object.keys(this.info);
       this.lbl.forEach(label => {
-        let formInfo = new AppFormInfo();
+        const formInfo = new AppFormInfo();
         formInfo.label = label;
         formInfo.fieldName = label;
         formInfo.val =  this.info[label];
 
-        for (var i = 0; i < label.length; i++) {
-          if(label.charAt(i) === label.charAt(i).toUpperCase()){
-              formInfo.label = label.substring(0,i)+' '+label.substring(i,label.length)
+        for (let i = 0; i < label.length; i++) {
+          if (label.charAt(i) === label.charAt(i).toUpperCase()){
+              formInfo.label = label.substring(0,i) + ' ' + label.substring(i, label.length);
           }
         }
         formInfo.label = formInfo.label.replace('_',' ');
-      
-        if(this.fieldCheck(label)){
-          _controller.formInfos.push(formInfo);
+        if (this.fieldCheck(label)) {
+          controller.formInfos.push(formInfo);
         }
-        
       });
     }
   }
-  public objectValue(label: string){
+  public objectValue(label: string) {
     return this.info[label];
   }
   public fieldCheck(f: string): boolean {
-    let isCheck =false;
-    this.field.forEach(fld =>{
-        if(fld ===f){
-          isCheck =true;
+    let isCheck = false;
+    this.field.forEach(fld => {
+        if (fld ===f) {
+          isCheck = true;
         }
     });
     return isCheck;
