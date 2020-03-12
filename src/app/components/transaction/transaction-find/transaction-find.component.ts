f553ce6861c99f31881a6c22a49c8ac1aba77e8e
M Uzeyr OZCAN <muzeyr@gmail.com>
2020-03-12 10:59:28 +0300

    Clean code

--- src/app/components/transaction/transaction-find/transaction-find.component.ts
+++ src/app/components/transaction/transaction-find/transaction-find.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { TransactionService } from 'src/app/services/transaction/transaction.service';
-import { TransactionClient } from 'src/app/models/transaction/transaction-client';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrManager } from 'ng6-toastr-notifications';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { TransactionClient } from 'src/app/models/transaction/transaction-client';
 import { TransactionDetail } from 'src/app/models/transaction/transaction-detail';
+import { TransactionService } from 'src/app/services/transaction/transaction.service';
 
 @Component({
   selector: 'app-transaction-find',
@@ -15,34 +14,29 @@ import { TransactionDetail } from 'src/app/models/transaction/transaction-detail
 export class TransactionFindComponent implements OnInit {
   public clientDetail: TransactionDetail;
   public operationLock = false;
+  public transactionClient: TransactionClient;
 
   constructor(private readonly ngxService: NgxUiLoaderService,
               private readonly activatedRouter: ActivatedRoute,
               public toastr: ToastrManager,
               private readonly router: Router,
               private readonly transactionService: TransactionService) {
-    }
+    this.transactionClient = new TransactionClient();
+    this.transactionClient.transactionId = this.activatedRouter.snapshot.params['id'];
+  }
 
   public ngOnInit(): void {
-    this.activatedRouter.paramMap.subscribe(params => {
-      this.ngxService.start();
-      const client = new TransactionClient();
-      client.transactionId = params.get('id');
-      this.transactionService.get(client).subscribe(data => {
-        if (data.status === 'DECLINED') {
-          this.toastr.warningToastr(data.message);
-          this.router.navigateByUrl('/transaction-query');
-          return;
 
-        }
-        this.clientDetail = data;
-        this.operationLock = true;
-        this.ngxService.stop();
-      });
-    }, error => {
+    this.transactionService.get(this.transactionClient).subscribe(data => {
+      if (data.status === 'DECLINED') {
+        this.toastr.warningToastr(data.message);
+        this.router.navigateByUrl('/transaction-query');
+        return;
+
+      }
+      this.clientDetail = data;
+      this.operationLock = true;
       this.ngxService.stop();
-      this.operationLock = false;
-      this.router.navigateByUrl('/transaction-query');
     });
   }
 }
